1073bdaaf2a49e63fce2e0678260812e06a94647
Cuarzo <j.m.decurnex@gmail.com>
2021-05-24 17:13:04 -0300

    theoldest terminado

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+	const año = new Date().toString().slice(11, -47)
+    let theOldest = array[0];
+	const getAge = (object) => {
+		let edad = 0
+        if (object.yearOfDeath) {
+            edad = object.yearOfDeath - object.yearOfBirth
+        } else {
+            edad = año - object.yearOfBirth
+        }
+        return edad
+    }
+    
+    for (let i=0; i<array.length; i++) {
+        if (getAge(array[i]) > getAge(theOldest)) {
+        	theOldest = array[i]
+        }
+    }
+    return theOldest.name
 };
 
 module.exports = findTheOldest;
